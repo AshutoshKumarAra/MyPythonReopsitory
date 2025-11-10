4b4002ea3344d1beb7a942fa5ad375115ae4cea1
Ashutosh Kumar <47491213+AshutoshKumarAra@users.noreply.github.com>
2025-11-10 08:44:04 +0530

    Include variable naming conventions in comments
    Added examples of variable naming conventions in Python.

--- 2.Variables.py
+++ 2.Variables.py
@@ -13,4 +13,7 @@ Things to be taken care of when you are creating variables:
 3. Variable names are case-sensitive (e.g., myVar and myvar are different variables).
 4. Avoid using Python reserved keywords as variable names (e.g., if, else, while, for, etc.).
 5. Do not use @ while defining variables. It works as a decorator. 
+6. PascalCaseFormat = AshutoshKumarAra (Yahan par humne Har word ko combine karke likha hai and har word ka first letter capital hota hai.)
+7. SnakeCaseFormat = Ashutosh_Kumar_Ara ( '_' ek snake ki tarah hota hai. Aise bhi aap variable define kar sakte hai)
+8. CamelCaseFormat = ashutoshKumarAra (Isme pehle word ka pehla letter small hota hai)
 """
